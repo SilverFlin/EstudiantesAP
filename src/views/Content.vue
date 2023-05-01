2e77720276c2813b9fd766e393739aabccd90d26
SilverFlin <toledorusso@outlook.com>
2023-04-30 23:02:39 -0700

    feat: contenido conoce mas

--- src/views/Content.vue
+++ src/views/Content.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0"
+    class="relative px-6 py-24 overflow-hidden bg-white isolate sm:py-32 lg:overflow-visible lg:px-0"
   >
-    <div class="absolute inset-0 -z-10 overflow-hidden">
+    <div class="absolute inset-0 overflow-hidden -z-10">
       <svg
         class="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
         aria-hidden="true"
@@ -34,7 +34,7 @@
       </svg>
     </div>
     <div
-      class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10"
+      class="grid max-w-2xl grid-cols-1 mx-auto gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10"
     >
       <div
         class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8"
@@ -42,27 +42,26 @@
         <div class="lg:pr-4">
           <div class="lg:max-w-lg">
             <p class="text-base font-semibold leading-7 text-indigo-600">
-              Deploy faster
+              Introducción
             </p>
             <h1
               class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
             >
-              A better workflow
+              Formas de Cuidado
             </h1>
             <p class="mt-6 text-xl leading-8 text-gray-700">
-              Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem.
-              At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at
-              vitae feugiat egestas.
+              De aquí en adelante hablaremos de algunas de las formas en la que
+              podemos reducir nuestra huella de carbono como estudiantes
             </p>
           </div>
         </div>
       </div>
       <div
-        class="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden"
+        class="p-12 -mt-12 -ml-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden"
       >
         <img
           class="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
-          src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+          src="https://images.unsplash.com/photo-1604631698209-c105c7874ea8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=416&q=80"
           alt=""
         />
       </div>
@@ -71,74 +70,233 @@
       >
         <div class="lg:pr-4">
           <div class="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
+            <h3
+              class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
+            >
+              Como reciclar una libreta
+            </h3>
             <p>
-              Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-              enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-              praesent donec est. Odio penatibus risus viverra tellus varius sit
-              neque erat velit. Faucibus commodo massa rhoncus, volutpat.
-              Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae
-              sed turpis id.
+              Normalmente cuesta acabar de usar una libreta, especialmente si es
+              algo gruesa. Usamos una hoja para una simple frase, quedan páginas
+              en blanco en medio y la mayoría están al final sin usar. Hay
+              diferentes ideas para poder reciclar estos objetos.
             </p>
             <ul role="list" class="mt-8 space-y-8 text-gray-600">
               <li class="flex gap-x-3">
                 <CloudArrowUpIcon
-                  class="mt-1 h-5 w-5 flex-none text-indigo-600"
+                  class="flex-none w-5 h-5 mt-1 text-indigo-600"
                   aria-hidden="true"
                 />
                 <span
                   ><strong class="font-semibold text-gray-900"
-                    >Push to deploy.</strong
+                    >Arrancar las páginas sobrantes y crear otra
+                    libreta.</strong
                   >
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Maiores impedit perferendis suscipit eaque, iste dolor
-                  cupiditate blanditiis ratione.</span
+                  Para obtener tus libretas personalizadas, una de las cosas que
+                  podemos hacer es arrancar las páginas o bien hojas sobrantes y
+                  dejarlas en una carpeta de anillas, por ejemplo. Así le darás
+                  otro uso a la libreta que está medio acabada</span
                 >
               </li>
               <li class="flex gap-x-3">
                 <LockClosedIcon
-                  class="mt-1 h-5 w-5 flex-none text-indigo-600"
+                  class="flex-none w-5 h-5 mt-1 text-indigo-600"
                   aria-hidden="true"
                 />
                 <span
                   ><strong class="font-semibold text-gray-900"
-                    >SSL certificates.</strong
+                    >Crear blocs.</strong
                   >
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                  qui lorem cupidatat commodo.</span
+                  Si la libreta es sin anillas tiene las hojas cosidas o
+                  grapadas, se pueden quitar las hojas con ayuda de un cutter y
+                  luego volver a juntarlas con grapas y taladros para crear un
+                  bloc. Si eres manitas y tienes imaginación, hasta puedes crear
+                  tu tapa de cartón reciclado y obtener así una libreta
+                  personalizada a tu gusto. <br />
+                  Con pocas hojas escritas hay otra manera todavía más sencilla
+                  de reciclar este objeto. Si hay pocas hojas escritas, cosa que
+                  puede pasar muchas veces, tan solo debemos arrancar estas y
+                  quedarnos con las hojas en blanco.</span
                 >
               </li>
               <li class="flex gap-x-3">
                 <ServerIcon
-                  class="mt-1 h-5 w-5 flex-none text-indigo-600"
+                  class="flex-none w-5 h-5 mt-1 text-indigo-600"
                   aria-hidden="true"
                 />
                 <span
                   ><strong class="font-semibold text-gray-900"
-                    >Database backups.</strong
+                    >¿Impresión o digital?</strong
                   >
-                  Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus.
-                  Et magna sit morbi lobortis.</span
+                  Muchos procedimientos o documentos se pueden producir y
+                  almacenar de forma digital y no necesariamente en físico.
+                  Siempre lleva contigo un USB que te permitirá guardar y llevar
+                  todos tus trabajos de forma virtual. Si de todas maneras debes
+                  imprimir, hazlo usando las dos caras de la hoja.</span
+                >
+              </li>
+              <li class="flex gap-x-3">
+                <LockClosedIcon
+                  class="flex-none w-5 h-5 mt-1 text-indigo-600"
+                  aria-hidden="true"
+                />
+                <span
+                  ><strong class="font-semibold text-gray-900"
+                    >Recicla en los centros de acopio.</strong
+                  >
+                  Otra forma de reciclar papel es llevándolo a los centros de
+                  acopio más cercanos a tu hogar o trabajo. Varias empresas,
+                  organismos del sector público y colegios se encargan de
+                  reciclar este y otros materiales para cuidar el medio
+                  ambiente.</span
+                >
+              </li>
+              <li class="flex gap-x-3">
+                <LockClosedIcon
+                  class="flex-none w-5 h-5 mt-1 text-indigo-600"
+                  aria-hidden="true"
+                />
+                <span
+                  ><strong class="font-semibold text-gray-900"
+                    >Reutiliza el periódico para cubrir o proteger
+                    superficies.</strong
+                  >
+                  Probablemente en tu casa tengas una buena cantidad de
+                  periódicos acumulados. Antes de echarlo a la basura, evalúa si
+                  puede servir para envolver algún adorno o artefacto que debes
+                  guardar, también puede servir para limpiar vidrios o cubrir el
+                  piso u otra superficie de manchas, pinturas, etc.</span
+                >
+              </li>
+              <li class="flex gap-x-3">
+                <LockClosedIcon
+                  class="flex-none w-5 h-5 mt-1 text-indigo-600"
+                  aria-hidden="true"
+                />
+                <span
+                  ><strong class="font-semibold text-gray-900"
+                    >Contagia en tu trabajo el hábito del reciclaje.</strong
+                  >
+                  Lleva a tu trabajo un contenedor para reciclar papel. Comunica
+                  a tus compañeros la función del recipiente y comparte con
+                  ellos otras formas de reducir el uso del papel. Por ejemplo,
+                  trabajar con boletas electrónicas o hacer sus propios
+                  cuadernos de notas con hojas recicladas.</span
+                >
+              </li>
+              <li class="flex gap-x-3">
+                <LockClosedIcon
+                  class="flex-none w-5 h-5 mt-1 text-indigo-600"
+                  aria-hidden="true"
+                />
+                <span
+                  ><strong class="font-semibold text-gray-900"
+                    >Si necesitas un cuaderno nuevo, opta por los cuadernos
+                    reciclados
+                  </strong>
+                  Cuando necesites un cuaderno u hojas nuevas, opta por comprar
+                  los reciclados. Es una de las mejores formas de seguir
+                  apoyando el reciclaje y el cuidado del medio ambiente.</span
                 >
               </li>
             </ul>
-            <p class="mt-8">
-              Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis
-              odio id et. Id blandit molestie auctor fermentum dignissim. Lacus
-              diam tincidunt ac cursus in vel. Mauris varius vulputate et
-              ultrices hac adipiscing egestas. Iaculis convallis ac tempor et
-              ut. Ac lorem vel integer orci.
+
+            <h3
+              class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
+            >
+              Viaje a la escuela
+            </h3>
+            <p class="mt-6 text-xl leading-8">
+              Estas opciones de transporte sostenible también ofrecen
+              oportunidades de actividad física que mejoran tu salud física y
+              mental. Por ejemplo, por cada milla que recorres andando, quemas
+              una media de 100 calorías.
             </p>
-            <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">
-              No server? No problem.
-            </h2>
-            <p class="mt-6">
-              Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam
-              consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod
-              vitae interdum mauris enim, consequat vulputate nibh. Maecenas
-              pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim
-              cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis
-              ipsum eu a sed convallis diam.
-            </p>
+            <ul role="list" class="mt-8 space-y-8 text-gray-600">
+              <li class="flex gap-x-3">
+                <CloudArrowUpIcon
+                  class="flex-none w-5 h-5 mt-1 text-indigo-600"
+                  aria-hidden="true"
+                />
+                <span
+                  ><strong class="font-semibold text-gray-900">XXXXX.</strong>
+                  Estando en un ambiente universitario, es normal que muchas
+                  personas ya tengan un automóvil propio, y es muy probable que
+                  esa persona seas tu, por lo que los siguientes consejos quizá
+                  sean de ayuda para ayudarte a reducir tu huella ecológica en
+                  la ciudad, estado o país</span
+                >
+              </li>
+              <li class="flex gap-x-3">
+                <CloudArrowUpIcon
+                  class="flex-none w-5 h-5 mt-1 text-indigo-600"
+                  aria-hidden="true"
+                />
+                <span
+                  ><strong class="font-semibold text-gray-900"
+                    >No dejes tu auto encendido .</strong
+                  >
+                  Si te vas a estacionar durante más de un minuto conviene
+                  detener el motor para dejar de emitir gases y evitar la
+                  contaminación. Hoy en día hay algunos coches que lo hacen de
+                  manera automática si consigues tráfico, pero si el tuyo no es
+                  de esos, te recomendamos hacerlo únicamente cuando estás fuera
+                  de la carretera.</span
+                >
+              </li>
+              <li class="flex gap-x-3">
+                <CloudArrowUpIcon
+                  class="flex-none w-5 h-5 mt-1 text-indigo-600"
+                  aria-hidden="true"
+                />
+                <span
+                  ><strong class="font-semibold text-gray-900"
+                    >Revisar el tubo de escape y el catalizador .</strong
+                  >
+                  El sistema de escape de los automóviles es el lugar donde se
+                  procura atrapar el máximo posible de sustancias nocivas
+                  derivadas de la combustión antes de ser emitidas a la
+                  atmósfera. Es por eso que es tan importante asegurarnos de que
+                  no hay fugas. Por otro lado, una combustión del motor
+                  defectuosa, puede producir mayor cantidad de humo.</span
+                >
+              </li>
+              <li class="flex gap-x-3">
+                <CloudArrowUpIcon
+                  class="flex-none w-5 h-5 mt-1 text-indigo-600"
+                  aria-hidden="true"
+                />
+                <span
+                  ><strong class="font-semibold text-gray-900"
+                    >Compartir auto</strong
+                  >
+                  Dependiendo de dónde vivas, puede que el transporte público no
+                  sea una opción para ti. Sin embargo, compartir auto es una
+                  gran alternativa igualmente eficaz. Algunas ciudades incluso
+                  han designado carriles de alta ocupación en las principales
+                  autopistas. Quienes comparten coche reducen las emisiones de
+                  gases de efecto invernadero asociadas a sus desplazamientos en
+                  aproximadamente un 5%. Aprovecha esta opción, teniendo el
+                  conocimiento de que mucha gente va a la universidad y es muy
+                  probable que concuerden con alguien en el mismo horario.
+                </span>
+              </li>
+              <li class="flex gap-x-3">
+                <CloudArrowUpIcon
+                  class="flex-none w-5 h-5 mt-1 text-indigo-600"
+                  aria-hidden="true"
+                />
+                <span
+                  ><strong class="font-semibold text-gray-900"
+                    >Muévete en bicicleta o andando
+                  </strong>
+                  Cuando tienes un carro, a menudo no piensas en utilizar medios
+                  de transporte alternativos, como caminar o ir en bicicleta.
+                  Caminar o ir en bici son alternativas estupendas cuando viajas
+                  a algún lugar cercano.
+                </span>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
